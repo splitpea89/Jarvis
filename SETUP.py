8c6fd9bacc734e416e1f196c31cf6fef7c3f7de5
splitpea89 <benashaheen@icloud.com>
2024-05-29 23:59:58 -0700

    finished gitignore for keys, added little helpful setup file

---
+++ SETUP.py
@@ -0,0 +1,8 @@
+def get_openai_key():
+    return("<YOUR-KEY>")
+
+def get_picovoice_key():
+    return("<YOUR-KEY>")
+
+
+# For this to run, you will need to plug in your keys, and then rename this file to "keys.py"
